078a1b616172a0cdb9e9f53f0745bd38d8ac813a
Gustavo Gomes <gustavo@alpine.my.domain>
2025-10-08 11:30:27 -0300

    Change hard links to Link componentents

--- frontend/src/components/Menu/Menu.jsx
+++ frontend/src/components/Menu/Menu.jsx
@@ -1,31 +1,25 @@
+import { Link } from 'react-router';
 import Profile from '../Profile/Profile'
 import './Menu.css'
 
 export default function Menu() {
 	return <div className='d-flex flex-column p-3 text-white bg-dark sidebar col-2'>
-		<a href="/" className='text-center mb-3'>
-			<span className='fs-4'>GOLD</span>
-		</a>
+		<Link to="/" className="nav-link" aria-current="page">
+			<span className='fs-4'>GOLD</span>	
+		</Link>
 		<ul className='nav flex-column mb-auto'>
 			<li className='nav-item hover-purple rounded'>
-				<a href="reunioes" className="nav-link" aria-current="page">
-					Reuniões
-				</a>
+				<Link to="/reunioes" className="nav-link" aria-current="page">Reuniões</Link>
 			</li>
 			<li className='nav-item hover-purple rounded'>
-				<a href="emprestimos" className="nav-link" aria-current="page">
-					Empréstimos
-				</a>
+				<Link to="/emprestimos" className="nav-link" aria-current="page">Empréstimos</Link>		
 			</li>
 			<li className='nav-item hover-purple rounded'>
-				<a href="graficos" className="nav-link" aria-current="page">
-					Gráficos
-				</a>
+				<Link to="/graficos" className="nav-link" aria-current="page">Gráficos</Link>
 			</li>
 			<li className='nav-item hover-purple rounded'>
-				<a href="membros" className="nav-link" aria-current="page">
-					Membros
-				</a>
+				<Link to="/membros" className="nav-link" aria-current="page">Membros</Link>
+				
 			</li>
 		</ul>
 
